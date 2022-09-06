35f71f669cf672119e9b96b20f5464d81c52f2d5
Ikedinachim <ikedinachimugochukwu@gmail.com>
2022-09-06 18:24:01 +0100

    added Redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Logo from '../UI/Logo';
+import classes from './Header.module.css'
+import { Menu, Close } from '@mui/icons-material';
+import { useSelector } from 'react-redux';
+
+
+
 
 const Header = () => {
+    const cars = useSelector(state => state.car.cars)
+    const[isDrawerOpen, openDrawer] = useState(false);
+    const openSideDrawer = () =>{
+        openDrawer(!isDrawerOpen);
+    }
     return (
-        <div>
-            header
+        <div className={classes.header}>
+            <a href="">
+                <Logo />
+            </a>
+            <div className={classes.menu}>
+              {cars && cars.map((car,index) => {
+                return     <a key={index} href="#">{car}</a>
+              })}
+
+            </div>
+            <div className={classes.rightMenu}>
+                <a href="#">Shop</a>
+                <a href="#">Tesla Account</a>
+
+            </div>
+            <Menu onClick={openSideDrawer } />
+          { isDrawerOpen &&  <div className={ isDrawerOpen ? classes.burgerNav: classes.burgerNavClose}>
+                <div className={classes.closeWrapper}>
+                <Close onClick={openSideDrawer} className={classes.closeIcon}/>
+                </div>
+                {cars && cars.map((car,index) => {
+                return    <li><a key={index} href="#">{car}</a></li>
+              })}
+                
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadstar</a></li>
+                <li><a href="#">Semi</a></li>
+                
+
+            </div>}
+            
+
         </div>
     );
 };
--- src/components/Section.js
+++ src/components/Section.js
@@ -2,15 +2,21 @@ import React from 'react';
 import Arrow from '../UI/Arrow';
 import classes from './Section.module.css';
 import Image from '../images/model-y.jpg';
+import { Fade } from 'react-awesome-reveal';
 const Section = ({title, description, leftBtnText, rightBtnText, backgroundImg}) => {
     
     return (
         <div className={classes.section} style={{backgroundImage:`url(${backgroundImg})`}} >
+            <Fade bottom>
             <div className={classes.itemText}>
                 <h1>{title}</h1>
                 <p>{description}</p>
             </div>
+
+            </Fade>
             <div className={classes.btnClass}>
+
+                <Fade bottom>
 
             <div className={classes.btnGroup}>
                 <div className={classes.leftBtn}>
@@ -22,6 +28,7 @@ const Section = ({title, description, leftBtnText, rightBtnText, backgroundImg})
                 </div>
                 }
             </div>
+                </Fade>
             <Arrow />
             </div>
         </div>
